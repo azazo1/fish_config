b3e3f31e8c4eb80d5228c2eba83f30493577a27f
azazo1 <azazo1@qq.com>
2025-10-22 23:34:27 +0800

    revert: uv link mode

--- config.fish
+++ config.fish
@@ -217,7 +217,7 @@ set -gx HOMEBREW_CORE_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homeb
 
 test ! -e "$HOME/.x-cmd.root/local/data/fish/rc.fish" || source "$HOME/.x-cmd.root/local/data/fish/rc.fish" # boot up x-cmd.
 set -gx UV_DEFAULT_INDEX 'https://pypi.tuna.tsinghua.edu.cn/simple'
-set -gx UV_LINK_MODE 'symlink'
+# set -gx UV_LINK_MODE 'symlink' # 如果使用软连接的话, 清除 uv 的缓存可能会导致项目环境被破坏.
 
 # bun {{{
 set --export BUN_INSTALL "$HOME/.bun"
