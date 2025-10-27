972eb0e4a2ec71f9ea21e38a27943769f2efc075
azazo1 <60778594+azazo1@users.noreply.github.com>
2025-10-28 00:36:37 +0800

    Add setup-nerd-font function for Nerd Font
    Add function to setup Nerd Font by downloading and installing it.

--- config.fish
+++ config.fish
@@ -34,6 +34,17 @@ if status is-interactive
     bind --user -s -M insert super-l accept-autosuggestion
     bind --user -s -M insert ctrl-j accept-autosuggestion
 
+    function setup-nerd-font
+        set -l font_filename "JetBrainsMonoNLNerdFontMono-Regular.ttf"
+        mkdir -p $HOME/tmp && pushd $HOME/tmp || begin; echo "failed to create tmp directory"; exit 1; end;
+        curl -LO -C - "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip"
+        unzip JetBrainsMono.zip $font_filename
+        echo "Copying font file $font_filename to /usr/share/fonts/myfonts"
+        sudo cp $font_filename /usr/share/fonts/myfonts && cd /usr/share/fonts/myfonts && sudo mkfontscale && sudo mkfontdir && sudo fc-cache
+        echo "Font $font_filename prepared"
+        popd
+    end
+
     function y
         set tmp (mktemp -t "yazi-cwd.XXXXXX")
         command yazi $argv --cwd-file="$tmp"
