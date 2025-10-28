901cbd1790d454d19a3e952d09558823daa45da4
azazo1 <azazo1@qq.com>
2025-10-28 21:28:59 +0800

    brew

--- config.fish
+++ config.fish
@@ -1,8 +1,10 @@
 fish_add_path --path "$HOME/.local/bin"
 fish_add_path --path "$HOME/scripts"
-fish_add_path --path /opt/homebrew/opt/util-linux/bin
-fish_add_path --path /opt/homebrew/opt/util-linux/sbin
+fish_add_path --path (brew --prefix util-linux)/bin
+fish_add_path --path (brew --prefix util-linux)/sbin
 fish_add_path --path (python3 -m site --user-base)/bin
+fish_add_path --path (brew --prefix e2fsprogs)/bin
+fish_add_path --path (brew --prefix e2fsprogs)/sbin
 
 set -gx JAVA_HOME /opt/homebrew/opt/openjdk
 set -gx CLASSPATH $JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar:.
