cbb98e21e4695d79906f8bf13fe55d88fc2dd70e
azazo1 <azazo1@qq.com>
2025-12-31 23:19:48 +0800

    howlto init

--- config.fish
+++ config.fish
@@ -20,6 +20,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
     zoxide init fish | source
+    command howlto --init | source
 
     alias ht 'howlto'
     alias nv 'nvim'
