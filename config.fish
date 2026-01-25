49282875b9ff5d7668adf1ace2b0dc2cb87ac777
azazo1 <azazo1@qq.com>
2026-01-25 11:55:37 +0800

    cherrypick from macos

--- config.fish
+++ config.fish
@@ -1,24 +1,28 @@
-set -gx PATH "$HOME/.local/bin:$PATH"
-set -gx PATH "$HOME/scripts:$PATH"
+fish_add_path --path "$HOME/.local/bin"
+fish_add_path --path "$HOME/scripts"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias update '. ~/.config/fish/config.fish'
-    alias config 'nvim ~/.config/fish/config.fish'
-    alias vconfig 'code ~/.config/fish/config.fish'
+    alias ht 'howlto'
+    alias update ". $__fish_config_dir/config.fish"
+    alias config "nvim $__fish_config_dir/config.fish"
+    alias vconfig "code $__fish_config_dir/config.fish"
     alias mp "multipass"
     alias activate "source ./.venv/bin/activate.fish"
     alias pg 'ps aux | command rg '
     alias rg "command rg -S --max-columns 1000"
     alias rgl "command rg -S"
-    alias mountd "sudo mount -r UUID='00460A96460A8C9A' /mnt/d"
-    alias mountc "sudo mount -r UUID='978E7B4F0DDA02C8' /mnt/c"
-    alias umountd "sudo umount /mnt/d"
-    alias umountc "sudo umount /mnt/c"
-    alias dockert 'docker run -v tmpapp:/app --rm -it'
+    alias dkt 'docker run -v tmpapp:/app --rm -it'
     alias ll 'ls -lha'
     alias l 'ls'
+    alias sl 'ls'
     alias lzd 'lazydocker'
+    alias dk 'docker'
     alias lg 'lazygit'
+    alias kg 'cargo'
+    alias lgnote 'lazygit -p ~/pjs/mynote'
+    alias configd 'cd $__fish_config_dir'
+
     fish_hybrid_key_bindings
     # Delete every ctrl-m ctrl-p ctrl-n key bindings.
     bind -e --preset -M insert ctrl-p ctrl-n
@@ -33,6 +37,29 @@ if status is-interactive
     bind --user -M visual ctrl-n down-or-search
     bind --user -s -M insert super-l accept-autosuggestion
     bind --user -s -M insert ctrl-j accept-autosuggestion
+
+    function _fish_dotenv_source
+        # First shell out to source the file in an isolated fashion. This is to
+        # ensure "atomicity" where either all settings as sourced or none at all.
+        if ! fish --private --no-config --command="source $FISH_DOTENV_FILE"
+            echo "dotenv: Error sourcing '$FISH_DOTENV_FILE' file, bailing." >&2
+            return 1
+        end
+
+        echo "dotenv: Sourcing '$FISH_DOTENV_FILE'" >&2
+        source $FISH_DOTENV_FILE
+    end
+    _fish_dotenv_source
+
+    function dugit --description "disk usage of new files in git staged"
+        set -l files (git diff --name-only --diff-filter=ARMC) (git diff --cached --name-only --diff-filter=ARMC)
+        set files (echo $files | sort | uniq)
+        if [ -z "$files" ]
+            echo dugit: No file to analyze.
+            return 1
+        end
+        command du -h -d 0 (string split ' ' $files)
+    end
 
     function setup-nerd-font
         set -l font_filename "JetBrainsMonoNLNerdFontMono-Regular.ttf"
@@ -55,12 +82,30 @@ if status is-interactive
     end
     alias yazi 'y'
 
-    function pd --description "pick a directory"
-        if [ (count $argv) -lt 1 ]
-            echo "pd: require an argument."
+    function pf --description "pick a file"
+        set -l args $argv
+        if [ (count $args) -lt 1 ]
+            echo -e "Usage: pf <file_search_pattern>"
             return 1
         end
-        set -l target (command fd $argv -t d | command fzf)
+        set -l target (command fd $args -t f | command fzf)
+        if not [ $status -eq 0 ]
+            echo "pf: user cancelled."
+            return 1
+        else if [ -z "$target" ]
+            echo "pf: target path is empty"
+            return 1
+        else
+            open $target
+        end
+    end
+
+    function pd --description "pick a directory"
+        set -l args $argv
+        if [ (count $args) -lt 1 ]
+            set args "."
+        end
+        set -l target (command fd $args -t d | command fzf)
         if not [ $status -eq 0 ]
             echo "pd: user cancelled."
             return 1
@@ -72,7 +117,6 @@ if status is-interactive
         end
     end
 
-
     function tmp --description 'create temp directory'
         set -l target_path
         if [ (count $argv) -gt 0 ]
@@ -82,6 +126,31 @@ if status is-interactive
         end
         command mkdir -p $target_path
         cd $target_path
+        ls
+    end
+    complete -c tmp -a '(fd . --max-depth 1 -t d ~/tmp -x basename)' -f
+
+    function mktmp --description 'create temp directory in system temp directory, remove dir when shell quit'
+        set -l tmp_path (command mktemp -d -t mktmp)
+        if [ (count $argv) -ge 1 ]
+            mkdir $tmp_path/$argv[1]
+            command fish -C "cd $tmp_path/$argv[1]"
+        else
+            command fish -C "cd $tmp_path"
+        end
+        pushd $tmp_path
+        for item in (command fd . $tmp_path -d 1)
+            set -l item_size (command du -h -d 0 $item | awk '{print $1}')
+            set item_disp (command fd -d 1 --color=always '^'$(basename $item)'$') # --full-path "^$(string trim -r -c '/' $item)\$")
+            echo '-' $item_disp (set_color yellow)$item_size(set_color normal)
+        end
+        popd
+        set -l tmp_size (command du -h -d 0 $tmp_path | cut -f 1)
+        if command rm -rf $tmp_path
+            echo (set_color green)Trashed(set_color normal) (dirname $tmp_path)/(set_color blue)(basename $tmp_path)(set_color normal)/ (set_color yellow)$tmp_size(set_color normal)
+        else
+            echo (set_color red)Not Trash(set_color normal) (dirname $tmp_path)/(set_color blue)(basename $tmp_path)(set_color normal)/ (set_color yellow)$tmp_size(set_color normal)
+        end
     end
 
     function setproxy
@@ -119,7 +188,10 @@ if status is-interactive
     # }
 
     # nvim {
-    set -gx PATH "/opt/nvim-linux-x86_64/bin:$PATH"
+    fish_add_path --path "/opt/nvim-linux-x86_64/bin"
+    if test -f "$(which nvim)"
+        set -gx EDITOR 'nvim'
+    end
     # }
 
     # x-cmd {
