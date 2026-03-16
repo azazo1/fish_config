229050f48be2dae5ab8b58c3b45741f139c0324f
azazo1 <azazo1@qq.com>
2026-03-16 15:05:05 +0800

    disable fzf integration

--- config.fish
+++ config.fish
@@ -352,7 +352,7 @@ if status is-interactive
         command launchctl list | command rg $domain
     end
 
-    command fzf --fish | source
+    # command fzf --fish | source
 end
 
 set -gx HOMEBREW_BREW_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
