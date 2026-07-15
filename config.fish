f9a557b74b99c02288b637f1e5c2dab228adf440
azazo1 <azazo1@qq.com>
2026-07-15 15:06:59 +0800

    howlto alias ignore args

--- config.fish
+++ config.fish
@@ -36,7 +36,7 @@ if status is-interactive
     command howlto --init | source
 
     alias sshcode 'codessh'
-    alias ht howlto
+    alias ht 'howlto --'
     alias pbc pbcopy
     alias pbp pbpaste
     alias pfc pfcopy
