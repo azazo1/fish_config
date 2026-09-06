556c302ac27b8f3ef050c44580d43d2218dd6389
azazo1 <azazo1@qq.com>
2026-09-06 21:09:08 +0800

    fix: diary-commit

--- functions/diary-commit.fish
+++ functions/diary-commit.fish
@@ -3,6 +3,7 @@
 function diary-commit
     # 直接对指定仓库执行 commit，只提交 diary/ 目录
     # -C 表示在指定路径执行 git，不改变当前 shell 目录
+    git -C ~/pjs/mynote add ~/pjs/mynote/diary/
     git -C ~/pjs/mynote commit -m "diary" -- diary/
     git -C ~/pjs/mynote push
     # 如果提交失败（比如没有更改），会返回非零，但不会中断函数
